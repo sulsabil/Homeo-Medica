4cbd673c8672dc721caf162ec0ddd2666bbb8fcc
SABILA <salsabilnishat@gmail.com>
2021-03-22 09:38:15 +0600

    2nd commit

--- admin/index.php
+++ admin/index.php
@@ -27,41 +27,29 @@ if (Session::get("user_type") == 'user') {
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
-    <meta name="description" content=""/>
-    <meta name="author" content=""/>
 
-    <title>Admin Dashboard || Animal Planet</title>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>Admin Dashboard || Homeo Medica</title>
 
     <!-- Bootstrap core CSS-->
-    <link
-            href="./resource/vendor/bootstrap/css/bootstrap.min.css"
-            rel="stylesheet"
-    />
+    <link href="./resource/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
 
     <!-- Custom fonts for this template-->
-    <link
-            href="./resource/vendor/fontawesome-free/css/all.min.css"
-            rel="stylesheet"
-            type="text/css"
-    />
+    <link href="./resource/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css" />
 
     <!-- Page level plugin CSS-->
-    <link
-            href="./resource/vendor/datatables/dataTables.bootstrap4.css"
-            rel="stylesheet"
-    />
+    <link href="./resource/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet" />
 
     <!-- Custom styles for this template-->
-    <link href="./resource/css/main.css" rel="stylesheet"/>
+    <link href="./resource/css/main.css" rel="stylesheet" />
 
-    <link href="./resource/css/custom.css" rel="stylesheet"/>
+    <link href="./resource/css/custom.css" rel="stylesheet" />
 
     <!--Script -->
     <!-- Bootstrap core JavaScript-->
@@ -85,199 +73,148 @@ if (Session::get("user_type") == 'user') {
 
 <body id="page-top">
 
-<nav class="navbar navbar-expand navbar-dark bg-dark static-top">
-    <a class="navbar-brand mr-1" href="index.php">Animal Planet</a>
+    <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+        <a class="navbar-brand mr-1" href="index.php">Homeo Medica</a>
 
-    <button
-            style="margin-left 5% !important "
-            class="btn btn-link btn-sm text-white order-1 order-sm-0"
-            id="sidebarToggle"
-            href="#"
-    >
-        <i class="fas fa-bars"></i>
-    </button>
 
-    <!-- Navbar Search -->
-    <div
-            class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0"
-    ></div>
 
-    <!-- Navbar -->
-    <ul class="navbar-nav ml-auto ml-md-0">
-        <li class="nav-item dropdown no-arrow mx-1">
-            <a
-                    class="nav-link dropdown-toggle"
-                    href="#"
-                    id="alertsDropdown"
-                    role="button"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-            >
-                <span class="badge badge-danger totalNoti"></span>
-                <i class="fas fa-bell fa-fw"></i>
-            </a>
-            <div
-                    class="dropdown-menu dropdown-menu-right notificationList"
-                    aria-labelledby="alertsDropdown">
-            </div>
-        </li>
-        <li class="nav-item dropdown no-arrow">
-            <a
-                    class="nav-link dropdown-toggle"
-                    href="#"
-                    id="userDropdown"
-                    role="button"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-            >
-                <img
-                        style="width: 30px; height: 30px"
-                        src="resource/img/user3.png"
-                        class="rounded-circle"
-                        alt="Avatar"
-                />
-                <span><?php echo Session::get("name"); ?></span>
-                <i class="icon-submenu lnr lnr-chevron-down"></i>
-            </a>
-            <div
-                    class="dropdown-menu dropdown-menu-right"
-                    aria-labelledby="userDropdown"
-            >
-                <div class="dropdown-divider"></div>
-                <a
-                        id="logoutBtn"
-                        class="dropdown-item"
-                        href="#"
-                        data-toggle="modal"
-                        data-target="#logoutModal"
-                >Logout</a
-                >
-            </div>
-        </li>
-    </ul>
-</nav>
+        <!-- Navbar Search -->
+        <div class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0"></div>
 
-<div id="wrapper">
-    <!-- Sidebar -->
-    <ul class="sidebar navbar-nav">
-        <li class="nav-item dropdown active">
-            <a
-                    class="nav-link"
-                    href="index.php"
-                    id="pagesDropdown"
-                    role="button"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-            >
-                <i class="fas fa-fw fa-tachometer-alt"></i>
-                <span>Dashboard</span>
-            </a>
-        </li>
+        <!-- Navbar -->
+        <ul class="navbar-nav ml-auto ml-md-0">
+            <li class="nav-item dropdown no-arrow mx-1">
+                <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="badge badge-danger totalNoti"></span>
+                    <i class="fas fa-bell fa-fw"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right notificationList" aria-labelledby="alertsDropdown">
+                </div>
+            </li>
+            <li class="nav-item dropdown no-arrow">
+                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img style="width: 30px; height: 30px" src="resource/img/user3.png" class="rounded-circle" alt="Avatar" />
+                    <span><?php echo Session::get("name"); ?></span>
+                    <i class="icon-submenu lnr lnr-chevron-down"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                    <div class="dropdown-divider"></div>
+                    <a id="logoutBtn" class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
+                </div>
+            </li>
+        </ul>
+    </nav>
 
-        <li class="nav-item">
-            <a class="nav-link" href="index.php?page=allDoctors">
-                <i class="fas fa-fw fa-table"></i>
-                <span>All Doctors</span>
-            </a>
-        </li>
+    <div id="wrapper">
+        <!-- Sidebar -->
+        <ul class="sidebar navbar-nav">
+            <li class="nav-item dropdown active">
+                <a class="nav-link" href="index.php" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
 
-        <li class="nav-item">
-            <a class="nav-link" href="index.php?page=addDoctor">
-                <i class="fas fa-fw fa-table"></i>
-                <span>Add Doctor</span>
-            </a>
-        </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?page=allDoctors">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>All Doctors</span>
+                </a>
+            </li>
 
-        <li class="nav-item">
-            <a class="nav-link" href="index.php?page=allHospitals">
-                <i class="fas fa-fw fa-table"></i>
-                <span>All Hospital</span>
-            </a>
-        </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?page=addDoctor">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Add Doctor</span>
+                </a>
+            </li>
 
-        <li class="nav-item">
-            <a class="nav-link" href="index.php?page=addHospital">
-                <i class="fas fa-fw fa-table"></i>
-                <span>Add Hospital</span>
-            </a>
-        </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?page=allHospitals">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>All Hospital</span>
+                </a>
+            </li>
 
-        <li class="nav-item">
-            <a class="nav-link" href="index.php?page=allPharmacy">
-                <i class="fas fa-fw fa-table"></i>
-                <span>All Pharmacy</span>
-            </a>
-        </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?page=addHospital">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Add Hospital</span>
+                </a>
+            </li>
 
-        <li class="nav-item">
-            <a class="nav-link" href="index.php?page=addPharmacy">
-                <i class="fas fa-fw fa-table"></i>
-                <span>Add Pharmacy</span>
-            </a>
-        </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?page=allPharmacy">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>All Pharmacy</span>
+                </a>
+            </li>
 
-        <li class="nav-item">
-            <a class="nav-link" href="index.php?page=allPosts">
-                <i class="fas fa-fw fa-table"></i>
-                <span>All Post</span>
-            </a>
-        </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?page=addPharmacy">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Add Pharmacy</span>
+                </a>
+            </li>
 
-    </ul>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?page=allPosts">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>All Post</span>
+                </a>
+            </li>
 
-    <!-- /.content-wrapper -->
+        </ul>
 
-    <div id="content-wrapper">
-        <?php
-        if (isset($_GET['page'])) {
-            $page = $_GET['page'] . '.php';
-        } else {
-            $page = "dashboard.php";
-        }
-        if (file_exists($page)) {
-            require_once $page;
-        }
-        ?>
+        <!-- /.content-wrapper -->
+
+        <div id="content-wrapper">
+            <?php
+            if (isset($_GET['page'])) {
+                $page = $_GET['page'] . '.php';
+            } else {
+                $page = "dashboard.php";
+            }
+            if (file_exists($page)) {
+                require_once $page;
+            }
+            ?>
+        </div>
+
     </div>
+    <!-- /#wrapper -->
 
-</div>
-<!-- /#wrapper -->
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
 
-<!-- Scroll to Top Button-->
-<a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-</a>
+    <script>
+        $(document).ready(function() {
+            $(document).on('click', '#logoutBtn', function() {
+                $.ajax({
+                    url: '../auth/logout.php',
+                    method: 'post',
+                    success: function(response) {
+                        console.log(response);
+                        window.location = 'login.php';
+                    }
+                });
+            });
 
-<script>
-    $(document).ready(function () {
-        $(document).on('click', '#logoutBtn', function () {
-            $.ajax({
-                url: '../auth/logout.php',
-                method: 'post',
-                success: function (response) {
-                    console.log(response);
-                    window.location = 'login.php';
-                }
+            var i;
+            $.get("../notification/index.php?userId=<?php echo Session::get('id'); ?>", function(data, status) {
+                var result = JSON.parse(data);
+                $('.totalNoti').text(result.notifications.length + "+");
+                $.each(result.notifications, function(key, value) {
+                    // console.log(value);
+                    // console.log(value)
+                    $('.notificationList').append('<a class="dropdown-item" href="index.php?page=singlePost&id=' + value.common_id + '">' + value.data + '</a>');
+                });
             });
         });
-
-        var i;
-        $.get("../notification/index.php?userId=<?php echo Session::get('id');?>", function (data, status) {
-            var result = JSON.parse(data);
-            $('.totalNoti').text(result.notifications.length+"+");
-            $.each(result.notifications, function (key, value) {
-                // console.log(value);
-                // console.log(value)
-                $('.notificationList').append('<a class="dropdown-item" href="index.php?page=singlePost&id=' + value.common_id + '">' + value.data + '</a>');
-            });
-        });
-    });
-
-</script>
+    </script>
 
 </body>
-</html>
 
+</html>
